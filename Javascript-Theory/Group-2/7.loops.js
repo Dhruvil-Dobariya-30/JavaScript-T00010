4df41c2709ed682b853094f149890b9ffed0c9b1
Dhruvil-Dobariya-30 <dhruvil.devoteeinfotech@gmail.com>
2024-07-14 17:36:45 +0530

    Group-2

---
+++ Javascript-Theory/Group-2/7.loops.js
@@ -0,0 +1,29 @@
+// For loop
+for (let i = 0; i <= 5; i++) {
+  console.log("The number is " + i);
+}
+
+for (let i = 5; i >= 1; i--) {
+  console.log("The Reverse number is " + i);
+}
+
+// While loop
+let j = 0;
+while (j < 5) {
+  console.log("The number is " + j);
+  j++;
+}
+
+// Do While loop
+let k = 6;
+do {
+  console.log(k);
+  k++;
+} while (k < 5);
+
+// let k = 1;
+// do {
+//   console.log(k);
+//   k++;
+// }
+// while (k < 5);
